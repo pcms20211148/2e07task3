e6ceec0623a31da23bb5eb5f4d82bb73593b6db5
pcms20211148 <115064897+pcms20211148@users.noreply.github.com>
2022-10-05 14:49:04 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showNumber(5)
+    basic.showNumber(6)
+})
 basic.forever(function () {
 	
 })
